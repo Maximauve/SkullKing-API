4e8330e6d0e3cc6b0279747da9b2cd1cc3bc5ec5
Mattox0 <matbegaaar@gmail.com>
2023-10-20 20:46:53 +0200

    :sparkles: Pirate glossery is OK

--- src/pirate-glossary/controller/pirate-glossary.controller.ts
+++ src/pirate-glossary/controller/pirate-glossary.controller.ts
@@ -4,6 +4,9 @@ import {JwtAuthGuard} from "../../auth/guards/jwt-auth.guard";
 import {PirateGlossaryService} from "../service/pirate-glossary.service";
 import {PirateGlossaryDto} from "../dto/pirate-glossary.dto";
 import {ValidationPipe} from "@nestjs/common/pipes";
+import {PirateGlossaryMultipleDto} from "../dto/pirate-glossary-multiple.dto";
+import {PirateGlossary} from "../pirate-glossary.entity";
+import {HttpException} from "@nestjs/common/exceptions";
 
 
 @Controller('pirate-glossary')
@@ -21,7 +24,17 @@ export class PirateGlossaryController {
   @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   @Post("/")
-  async Create(@Body() pirateGlossary: PirateGlossaryDto): Promise<{}> {
-    return this.pirateGlossaryService.Create(pirateGlossary);
+  async Create(@Body() pirateGlossary: PirateGlossaryDto | PirateGlossaryMultipleDto): Promise<PirateGlossary[] | PirateGlossary> {
+    if (Array.isArray(pirateGlossary.word)) {
+      let allPirates: PirateGlossary[] = [];
+      for (const w of pirateGlossary.word) {
+        if (await this.pirateGlossaryService.checkWord(w)) continue;
+        allPirates.push(await this.pirateGlossaryService.Create({word: w} as PirateGlossaryDto));
+      }
+      if (allPirates.length <= 0) throw new HttpException('Words already exists', 409);
+      return allPirates;
+    }
+    if (await this.pirateGlossaryService.checkWord(pirateGlossary.word)) throw new HttpException('Word already exists', 409);
+    return this.pirateGlossaryService.Create(pirateGlossary as PirateGlossaryDto);
   }
 }
--- src/pirate-glossary/service/pirate-glossary.service.ts
+++ src/pirate-glossary/service/pirate-glossary.service.ts
@@ -4,28 +4,42 @@ import {Repository} from 'typeorm';
 import {PirateGlossary} from "../pirate-glossary.entity";
 import {PirateGlossaryDto} from "../dto/pirate-glossary.dto";
 import {PirateGlossaryController} from "../controller/pirate-glossary.controller";
+import {HttpException} from "@nestjs/common/exceptions";
 
 @Injectable()
 export class PirateGlossaryService {
   constructor(
     @InjectRepository(PirateGlossary)
-    private usersRepository: Repository<PirateGlossary>,
+    private pirateGlossaryRepository: Repository<PirateGlossary>,
   ) {}
 
   async GetThreeWord(): Promise<string> {
-    return pickRandomElements(await this.usersRepository.find());
+    const allWords = await this.pirateGlossaryRepository.find();
+    if (allWords.length < 3) throw new HttpException('Not enough words in the database', 404);
+    return pickRandomElements(allWords);
+  }
+
+  async FindOneWord(word: string): Promise<PirateGlossary> {
+    return await this.pirateGlossaryRepository
+      .createQueryBuilder("pirate_glossary")
+      .where("pirate_glossary.word = :word", {word: word})
+      .getOne()
   }
 
   async Create(pirateGlossary: PirateGlossaryDto): Promise<PirateGlossary> {
-    const newPirateGlossary = this.usersRepository.create(pirateGlossary);
-    return this.usersRepository.save(newPirateGlossary);
+    const newPirateGlossary = this.pirateGlossaryRepository.create(pirateGlossary);
+    return this.pirateGlossaryRepository.save(newPirateGlossary);
+  }
+
+  async checkWord(word: string): Promise<boolean> {
+    const pirateGlossary = await this.FindOneWord(word);
+    return !!pirateGlossary;
   }
 }
 
 function pickRandomElements(arr: PirateGlossary[]): string {
   const randomElements: PirateGlossary[] = [];
   const shuffledArray = [...arr];
-
   while (randomElements.length < 3) {
     randomElements.push(shuffledArray.splice(Math.floor(Math.random() * shuffledArray.length), 1)[0]);
   }
