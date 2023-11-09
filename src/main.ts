55f88ee26155d960a965ccc5e588b40b5bee2c19
Maximauve <max.mourgues@gmail.com>
2023-11-09 21:20:56 +0100

    :bricks: Adding PORT for heroku

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,  { cors: true });
 
   // await app.startAllMicroservices();
-  await app.listen(5019);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
