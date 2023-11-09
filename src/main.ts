f831a204012626d17ae7bd91fa18faf5bb7e9ec2
MMOURGUES <maxime.mourgues@ynov.com>
2023-11-09 16:35:31 +0100

    :bricks: Vercel si ça marche pas c'est la faute de jérémy

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,  { cors: true });
 
   await app.startAllMicroservices();
-  await app.init();
+  await app.listen(5019);
 }
 bootstrap();
-
-export const handler: any = async (
-    event: any,
-    context: any,
-    callback: any,
-) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
