19b93c009d34fd734a7ff06047f3e3bde69c3715
Mattox0 <matbegaaar@gmail.com>
2023-11-14 00:13:55 +0100

    :construction: Upgrade websocket

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -3,9 +3,10 @@ import { GameController } from './controller/game.controller';
 import {forwardRef} from "@nestjs/common/utils";
 import {RedisModule} from "../redis/redis.module";
 import {GameService} from "./service/game.service";
+import {RoomModule} from "../room/room.module";
 
 @Module({
-  imports: [forwardRef(() => RedisModule)],
+  imports: [forwardRef(() => RedisModule), forwardRef(() => RoomModule)],
   controllers: [GameController],
   providers: [GameService],
   exports: [GameService],
